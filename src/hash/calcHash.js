bd620a0c8c2c482ec47a40168f93e7d61e4ad2cd
Stas Smoliar <75983960+fpastl@users.noreply.github.com>
2022-06-04 02:14:01 +0300

    feat: implement functions for hash part

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,14 @@
+import crypto from 'crypto';
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname  = path.dirname(__filename);
+
+    const hashSum = crypto.createHash('sha256');
+    hashSum.update(fs.readFileSync(path.join(__dirname,'files','fileToCalculateHashFor.txt')));
+    const hex = hashSum.digest('hex');
+    return hex;
 };
