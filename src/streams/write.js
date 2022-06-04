77148da8fc6b41bd580a9d91a48eb6bf158dce67
Stas Smoliar <75983960+fpastl@users.noreply.github.com>
2022-06-04 03:13:38 +0300

    feat: implement functions for streams part

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,26 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { pipeline } from 'stream';
+import {fileURLToPath} from 'url';
+
 export const write = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname  = path.dirname(__filename);
+
+    fs.open(path.join(__dirname,'files','fileToWrite.txt'),'a')
+    .then(
+        (file) => 
+        {
+            let writeableStream = file.createWriteStream({encoding: 'UTF-8'});
+            pipeline(
+                process.stdin, 
+                writeableStream,  
+                (err) => {
+                    if (err) {
+                    console.error('Pipeline failed.', err);
+                    }
+                }
+            );
+        }
+    )
 };
