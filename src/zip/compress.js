706a8becdc9d4d497124bc517e3f478a30130335
Stas Smoliar <75983960+fpastl@users.noreply.github.com>
2022-06-04 10:11:06 +0300

    feat: implement functions for zip part

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,14 @@
+import { createGzip } from 'zlib';
+import * as fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
 export const compress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname  = path.dirname(__filename);
+
+    const gzip = createGzip();
+    fs.createReadStream(path.join(__dirname,'files','fileToCompress.txt'))
+    .pipe(gzip)
+    .pipe(fs.createWriteStream(path.join(__dirname,'files','archive.gz')));
 };
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,14 @@
+import { createUnzip } from 'zlib';
+import * as fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
 export const decompress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname  = path.dirname(__filename);
+
+    const unzip = createUnzip();
+    fs.createReadStream(path.join(__dirname,'files','archive.gz'))
+    .pipe(unzip)
+    .pipe(fs.createWriteStream(path.join(__dirname,'files','fileToCompress.txt')));
 };
